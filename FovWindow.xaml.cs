21b5f65dc8b8b298dc4db635a53061ed27457e5e
XeinTDM <mjaow2020@gmail.com>
2023-08-22 16:54:39 +0200

    .

--- FovWindow.xaml.cs
+++ FovWindow.xaml.cs
@@ -16,9 +16,7 @@ namespace ColorBrother
         {
             InitializeComponent();
             PositionOverlay();
-            _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(10) };
-            _timer.Tick += Timer_Tick;
-            _timer.Start();
+            MonitorRegion();
         }
 
         private void PositionOverlay()
@@ -28,53 +26,79 @@ namespace ColorBrother
             Top = (SystemParameters.PrimaryScreenHeight - Height) / 2;
         }
 
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            if (Mouse.RightButton == MouseButtonState.Pressed)
-            {
-                var screenBitmap = CaptureScreen((int)Left, (int)Top, (int)Width, (int)Height);
-                var targetColor = ColorTranslator.FromHtml("#e5ed02"); // Target color
-
-                for (int y = 0; y < screenBitmap.Height; y++)
-                {
-                    for (int x = 0; x < screenBitmap.Width; x++)
-                    {
-                        var pixelColor = screenBitmap.GetPixel(x, y);
-                        bool isMatch = IsColorMatch(pixelColor, targetColor); // Check if colors match
-
-                        if (isMatch)
-                        {
-                            SetCursorPos((int)Left + x, (int)Top + y);
-                            return;
-                        }
-                    }
-                }
-            }
-        }
-
         private bool IsColorMatch(Color color1, Color color2)
         {
-            // Calculate the Euclidean distance between the two colors
             int redDifference = color1.R - color2.R;
             int greenDifference = color1.G - color2.G;
             int blueDifference = color1.B - color2.B;
             double distance = Math.Sqrt(redDifference * redDifference + greenDifference * greenDifference + blueDifference * blueDifference);
-
-            // Define a threshold for what constitutes a "close match"
-            // You can adjust this value based on your specific requirements
             const double threshold = 50.0;
-
             return distance < threshold;
         }
 
-        private Bitmap CaptureScreen(int left, int top, int width, int height)
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr CreateEllipticRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr SelectObject(IntPtr hdc, IntPtr h);
+
+        [DllImport("user32.dll")]
+        private static extern int GetPixel(IntPtr hdc, int nXPos, int nYPos);
+
+        private Timer _monitorTimer;
+        private Color _previousColor;
+
+        private void MonitorRegion()
         {
-            var bitmap = new Bitmap(width, height);
-            using (var graphics = Graphics.FromImage(bitmap))
+            // Define the region to monitor (adjust the coordinates as needed)
+            IntPtr region = CreateEllipticRgn((int)Left, (int)Top, (int)(Left + Width), (int)(Top + Height));
+
+            // Get the device context for the screen
+            IntPtr hdc = GetDC(IntPtr.Zero);
+
+            // Select the region into the device context
+            SelectObject(hdc, region);
+
+            // Initialize the previous color
+            _previousColor = GetPixelColor(hdc, (int)Left + 50, (int)Top + 50);
+
+            // Start the timer to monitor the region for changes
+            _monitorTimer = new Timer(MonitorRegionCallback, hdc, 0, 100); // Check every 100 milliseconds
+        }
+
+        private void MonitorRegionCallback(object state)
+        {
+            IntPtr hdc = (IntPtr)state;
+
+            // Check the color of a pixel within the region
+            Color currentColor = GetPixelColor(hdc, (int)Left + 50, (int)Top + 50);
+
+            // Check if the color has changed
+            if (currentColor != _previousColor)
             {
-                graphics.CopyFromScreen(left, top, 0, 0, new System.Drawing.Size(width, height));
+                // The color has changed, so check if it matches the target color
+                Color targetColor = ColorTranslator.FromHtml("#e5ed02");
+                if (IsColorMatch(currentColor, targetColor))
+                {
+                    // Move the mouse to the target color's position
+                    SetCursorPos((int)Left + 50, (int)Top + 50);
+                }
+
+                // Update the previous color
+                _previousColor = currentColor;
             }
-            return bitmap;
+        }
+
+        private Color GetPixelColor(IntPtr hdc, int x, int y)
+        {
+            int colorRef = GetPixel(hdc, x, y);
+            int red = colorRef & 0xFF;
+            int green = (colorRef >> 8) & 0xFF;
+            int blue = (colorRef >> 16) & 0xFF;
+            return Color.FromArgb(red, green, blue);
         }
 
         [DllImport("user32.dll")]
